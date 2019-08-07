ea64774d5a784b611aa1e5d9a3bac118c1c6826e
Andy Gertjejansen <andygertjejansen@gmail.com>
2019-08-07 14:46:11 -0500

    Remove remote state for those following around at home

--- terraform/init.tf
+++ terraform/init.tf
@@ -3,11 +3,3 @@ provider okta {}
 
 provider aws {}
 
-terraform {
-  backend "s3" {
-    bucket                  = "terraform-state-689543204258-us-east-1"
-    key                     = "terraform-provider-okta-demos/terraform.tfstate"
-    region                  = "us-east-1"
-    shared_credentials_file = "/root/.aws/creds"
-  }
-}
